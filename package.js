c892b339d38541bb6384562d30474cb4007f45a9
Abigail Watson <awatson@mitre.org>
2022-09-18 15:12:04 -0400

    sync

--- package.js
+++ package.js
@@ -12,14 +12,14 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('2.7.3');
-  api.use('ecmascript');
+  api.use('ecmascript@0.16.0');
   api.use('clinical:json-routes');
   api.use('clinical:vault-server');
   api.addFiles('routes.js', 'server');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
+  api.use('ecmascript@0.16.0');
   api.use('tinytest');
   api.use('hl7-fast:identity-matching-module');
   api.mainModule('identity-matching-module-tests.js');
